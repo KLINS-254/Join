6169c5037c30b318cfb1460d86458cc5bc862335
KLINS-254 <klinswoo@gmail.com>
2026-07-20 02:44:24 +0300

    Add bus schedule data to buses.js
    Added a list of bus schedules including details such as company, type, route, departure, arrival, duration, price, rating, and seats left.

---
+++ database/buses.js
@@ -0,0 +1,159 @@
+const buses = [
+
+{
+id:1,
+company:"ENA Coach",
+type:"Luxury",
+route:"Nairobi-Kisumu",
+departure:"07:00 AM",
+arrival:"05:45 PM",
+duration:"10h 45m",
+price:1800,
+rating:4.3,
+seatsLeft:45
+},
+
+{
+id:2,
+company:"ENA Coach",
+type:"Luxury",
+route:"Nairobi-Kisumu",
+departure:"07:30 AM",
+arrival:"07:25 PM",
+duration:"11h 55m",
+price:1800,
+rating:4.4,
+seatsLeft:40
+},
+
+{
+id:3,
+company:"ENA Coach",
+type:"Luxury",
+route:"Nairobi-Kisumu",
+departure:"08:00 AM",
+arrival:"06:15 PM",
+duration:"10h 15m",
+price:1850,
+rating:4.5,
+seatsLeft:28
+},
+
+{
+id:4,
+company:"ENA Coach",
+type:"Executive",
+route:"Nairobi-Kisumu",
+departure:"08:30 AM",
+arrival:"05:55 PM",
+duration:"09h 25m",
+price:2300,
+rating:4.8,
+seatsLeft:18
+},
+
+{
+id:5,
+company:"ENA Coach",
+type:"Executive",
+route:"Nairobi-Kisumu",
+departure:"09:00 AM",
+arrival:"06:00 PM",
+duration:"09h 00m",
+price:2500,
+rating:4.9,
+seatsLeft:16
+},
+
+{
+id:6,
+company:"ENA Coach",
+type:"Executive",
+route:"Nairobi-Busia",
+departure:"09:30 AM",
+arrival:"07:15 PM",
+duration:"09h 45m",
+price:2400,
+rating:4.8,
+seatsLeft:22
+},
+
+{
+id:7,
+company:"ENA Coach",
+type:"Nissan",
+route:"Nairobi-Kisumu",
+departure:"06:00 AM",
+arrival:"02:30 PM",
+duration:"08h 30m",
+price:1500,
+rating:4.2,
+seatsLeft:12
+},
+
+{
+id:8,
+company:"ENA Coach",
+type:"Nissan",
+route:"Nairobi-Kisumu",
+departure:"10:00 AM",
+arrival:"06:20 PM",
+duration:"08h 20m",
+price:1500,
+rating:4.3,
+seatsLeft:10
+},
+
+{
+id:9,
+company:"ENA Coach",
+type:"Nissan",
+route:"Nairobi-Busia",
+departure:"11:00 AM",
+arrival:"08:10 PM",
+duration:"09h 10m",
+price:1700,
+rating:4.5,
+seatsLeft:8
+},
+
+{
+id:10,
+company:"ENA Coach",
+type:"Luxury",
+route:"Nairobi-Eldoret",
+departure:"07:15 AM",
+arrival:"02:45 PM",
+duration:"07h 30m",
+price:1700,
+rating:4.4,
+seatsLeft:30
+},
+
+{
+id:11,
+company:"ENA Coach",
+type:"Executive",
+route:"Nairobi-Eldoret",
+departure:"09:15 AM",
+arrival:"04:35 PM",
+duration:"07h 20m",
+price:2200,
+rating:4.8,
+seatsLeft:20
+},
+
+{
+id:12,
+company:"ENA Coach",
+type:"Nissan",
+route:"Nairobi-Eldoret",
+departure:"12:00 PM",
+arrival:"07:40 PM",
+duration:"07h 40m",
+price:1600,
+rating:4.3,
+seatsLeft:9
+}
+
+];
